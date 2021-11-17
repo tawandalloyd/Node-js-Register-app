49f9fc45562d8b20b15958548c5c4959eeae265c
tawandalloyd <51161179+tawandalloyd@users.noreply.github.com>
2021-11-17 16:48:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -251,8 +251,8 @@ router.route('/resendOTP').post(function(req,res){
                         service : 'Gmail',
 
                         auth: {
-                           user: 'tawandalloydcharuka@gmail.com',
-                           pass:'6209lolo',
+                           user: 'email',
+                           pass:'password',
                         }
              })
             var mailOptions = {
